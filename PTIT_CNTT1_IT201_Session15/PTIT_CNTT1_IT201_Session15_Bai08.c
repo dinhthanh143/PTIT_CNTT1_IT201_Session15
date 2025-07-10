bbbe14c77b71ad2546a279136a7374b3bbd39059
dinhthanh143 <neonlimea@gmail.com>
2025-07-10 20:52:34 +0700

    Update PTIT_CNTT1_IT201_Session15_Bai08.c

--- PTIT_CNTT1_IT201_Session15/PTIT_CNTT1_IT201_Session15_Bai08.c
+++ PTIT_CNTT1_IT201_Session15/PTIT_CNTT1_IT201_Session15_Bai08.c
@@ -3,15 +3,20 @@
 #include<math.h>
 #include<stdlib.h>
 #include<stdbool.h>
+#define MAX 100
+typedef struct Customer {
+   int age;
+   char name[30];
+}Customer;
 typedef struct Queue {
-   int *arr;
+   Customer *arr;
    int front;
    int rear;
    int cap;
 }Queue;
 Queue *createQueue(int cap) {
    Queue *q = (Queue*)malloc(sizeof(Queue));
-   q->arr = (int*)malloc(sizeof(int)*cap);
+   q->arr = (Customer*)malloc(sizeof(Customer)*cap);
    q->front = 0;
    q->rear = -1;
    return q;
@@ -22,28 +27,30 @@ int isEmpty(Queue *q) {
 int isFull(Queue *q) {
    return q->rear == q->cap - 1;
 }
-void enQueue(Queue *q, int x) {
+void enQueue(Queue *q, int x, char name[]) {
    if (isFull(q)) {
       printf("\nHang doi da full\n");
       return;
    }
    q->rear++;
-   q->arr[q->rear] = x;
+   q->arr[q->rear].age = x;
+   strcpy(q->arr[q->rear].name,name);
 }
-int deQueue(Queue *old, Queue *nor) {
+void deQueue(Queue *old, Queue *nor) {
    if (isEmpty(old) && isEmpty(nor)) {
       printf("\nCa hai hang doi dang trong\n");
-      return -1;
+      return;
    }
    int value;
    if (isEmpty(old) && !isEmpty(nor)) {
-      value = nor->arr[nor->front];
+      value = nor->arr[nor->front].age;
+      printf("\nDa phuc vu khac hang %s, %d tuoi", nor->arr[nor->front].name,value);
       nor->front++;
    }else {
-      value = old->arr[old->front];
+      value = old->arr[old->front].age;
+      printf("\nDa phuc vu khac hang %s, %d tuoi", old->arr[old->front].name,value);
       old->front++;
    }
-   return value;
 }
 void freeQueue(Queue *q) {
    free(q->arr);
@@ -55,9 +62,14 @@ int main(){
    printf("Moi nhap so luong cho hang doi: ");
    scanf("%d",&size);
    int arr[size];
+   char name[MAX][30];
    for(int i=0;i<size;i++) {
+      printf("Nhap ten cho khac hang thu %d: ",i+1);
+      fgets(name[i],30,stdin);
+      name[i][strcspn(name[i],"\n")] = 0;
       printf("Moi nhap do tuoi cho khach hang thu %d: ",i+1);
       scanf("%d",&arr[i]);
+      getchar();
       if (arr[i]<60) {
          normalSize++;;
       }
@@ -66,9 +78,9 @@ int main(){
    Queue *oldQueue = createQueue(size - normalSize);
    for (int i = 0; i < size; i++) {
       if (arr[i]>=60) {
-         enQueue(oldQueue, arr[i]);
+         enQueue(oldQueue, arr[i],name[i]);
       }else {
-         enQueue(normalQueue, arr[i]);
+         enQueue(normalQueue, arr[i],name[i]);
       }
    }
    freeQueue(oldQueue);
